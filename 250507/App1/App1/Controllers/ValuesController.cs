a4e2c6abd1566f3a5f90473066946dff6bb0a0be
DuaDepTrai <hieun.mgc@gmail.com>
2025-05-12 18:32:46 +0700

    laptop

---
+++ 250507/App1/App1/Controllers/ValuesController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace App1.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        // GET api/values
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        public string Post(string value)
+        {
+            return value;
+        }
+
+        // PUT api/values/5
+        public string Put(int id, string value)
+        {
+            return value;
+        }
+
+        // DELETE api/values/5
+        public int Delete(int id)
+        {
+            return id;
+        }
+    }
+}
